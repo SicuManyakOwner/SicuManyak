284ff5b937f75c3abdc543fe9e151c58396b5f1a
halavluv <guywolf2@gmail.com>
2017-11-07 12:52:22 +0200

    Starting to sort the GUI thing out, added very basic menu

--- src/gui.py
+++ src/gui.py
@@ -4,18 +4,39 @@
 import Tkinter
 
 
-def sikumim_page(window):
+WINDOW = Tkinter.Tk()
+
+
+def clean_page(window, location):
     widgets = window.pack_slaves()
     for widget in widgets:
         widget.destroy()
+    if location == "sikumim":
+        sikumim_page(WINDOW)
+    elif location == "premium":
+        premium_page(WINDOW)
+    elif location == "main":
+        main()
+
+
+def sikumim_page(window):
+    label1 = Tkinter.Label(window, text=u"סיכומים"[::-1]).pack()
+    return_button = Tkinter.Button(WINDOW, text=u"חזרה למסך הראשי"[::-1], command=lambda: clean_page(WINDOW, "main")).pack()
+    window.mainloop()
+
+
+def premium_page(window):
+    label1 = Tkinter.Label(window, text=u"קנה פרמיום"[::-1]).pack()
+    return_button = Tkinter.Button(WINDOW, text=u"חזרה למסך הראשי"[::-1], command=lambda: clean_page(WINDOW, "main")).pack()
     window.mainloop()
 
 
 def main():
-    window = Tkinter.Tk()
     sikumim = u"סיכומים"
-    sikumim_button = Tkinter.Button(window, text=sikumim[::-1], command=lambda: sikumim_page(window)).pack()
-    window.mainloop()
+    premium = u"קנה פרמיום"
+    sikumim_button = Tkinter.Button(WINDOW, text=sikumim[::-1], command=lambda: clean_page(WINDOW, "sikumim")).pack()
+    premium_button = Tkinter.Button(WINDOW, text=premium[::-1], command=lambda: clean_page(WINDOW, "premium")).pack()
+    WINDOW.mainloop()
 
 
 if __name__ == "__main__":

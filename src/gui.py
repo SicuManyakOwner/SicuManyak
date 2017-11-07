e73587e3ca298290eaa46d946d377130e58173fe
halavluv <guywolf2@gmail.com>
2017-11-07 15:33:43 +0200

    Docstrings and stuff IDK

--- src/gui.py
+++ src/gui.py
@@ -8,36 +8,46 @@ WINDOW = Tkinter.Tk()
 
 
 def clean_page(window, location):
-    widgets = window.pack_slaves()
+    """Cleans the window of modules to prepare for a new page"""
+    widgets = window.grid_slaves()
     for widget in widgets:
         widget.destroy()
     if location == "sikumim":
-        sikumim_page(WINDOW)
+        sikumim_page(window)
     elif location == "premium":
-        premium_page(WINDOW)
+        premium_page(window)
     elif location == "main":
-        main()
+        main(window)
 
 
 def sikumim_page(window):
-    label1 = Tkinter.Label(window, text=u"סיכומים"[::-1]).pack()
-    return_button = Tkinter.Button(WINDOW, text=u"חזרה למסך הראשי"[::-1], command=lambda: clean_page(WINDOW, "main")).pack()
+    """Opens the sikumim page"""
+    window.title(u"סיכומים")
+    Tkinter.Label(window, text=u"סיכומים"[::-1]).grid(row=1, column=1)
+    Tkinter.Button(WINDOW, text=u"חזרה למסך הראשי"[::-1],
+                   command=lambda: clean_page(WINDOW, "main")).grid(row=2, column=1)
     window.mainloop()
 
 
 def premium_page(window):
-    label1 = Tkinter.Label(window, text=u"קנה פרמיום"[::-1]).pack()
-    return_button = Tkinter.Button(WINDOW, text=u"חזרה למסך הראשי"[::-1], command=lambda: clean_page(WINDOW, "main")).pack()
+    """Open the "Buy Premium" page"""
+    window.title(u"קנה פרמיום")
+    Tkinter.Label(window, text=u"קנה פרמיום"[::-1]).grid(row=1, column=1)
+    Tkinter.Button(WINDOW, text=u"חזרה למסך הראשי"[::-1],
+                   command=lambda: clean_page(WINDOW, "main")).grid(row=2, column=1)
     window.mainloop()
 
 
-def main():
+def main(window):
+    window.title(u"ברוכים הבאים לסיכומניאק!")
     sikumim = u"סיכומים"
     premium = u"קנה פרמיום"
-    sikumim_button = Tkinter.Button(WINDOW, text=sikumim[::-1], command=lambda: clean_page(WINDOW, "sikumim")).pack()
-    premium_button = Tkinter.Button(WINDOW, text=premium[::-1], command=lambda: clean_page(WINDOW, "premium")).pack()
+    Tkinter.Button(window, text=sikumim[::-1],
+                   command=lambda: clean_page(window, "sikumim")).grid(row=1, column=1)
+    Tkinter.Button(window, text=premium[::-1], fg="#ff0000",
+                   command=lambda: clean_page(window, "premium")).grid(row=2, column=1)
     WINDOW.mainloop()
 
 
 if __name__ == "__main__":
-    main()
+    main(WINDOW)

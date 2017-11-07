661fbcaeaa729b69b27ef4ba47e9bcf9f949bb95
halavluv <guywolf2@gmail.com>
2017-11-07 11:48:22 +0200

    First window prototype

---
+++ src/gui.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import Tkinter
+def sikumim_page():
+
+def main():
+    window = Tkinter.Tk()
+    sikumim = u"סיכומים"
+    Tkinter.Button(window, text=sikumim[::-1], command = sikumim_page()).pack()
+    window.mainloop()
+
+
+if __name__ == "__main__":
+    main()

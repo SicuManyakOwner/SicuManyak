11487bc76b91d96c5693a656467fd12391d44400
halavluv <guywolf2@gmail.com>
2017-11-08 10:55:24 +0200

    Added basic server integration, small optimizations.

--- src/gui.py
+++ src/gui.py
@@ -1,53 +1,70 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import Tkinter
+from Tkinter import *
+import contact_server
+
+WINDOW = Tk()
 
 
-WINDOW = Tkinter.Tk()
-
-
-def clean_page(window, location):
+def clean_page(location):
     """Cleans the window of modules to prepare for a new page"""
-    widgets = window.grid_slaves()
+    widgets = WINDOW.grid_slaves()
     for widget in widgets:
         widget.destroy()
     if location == "sikumim":
-        sikumim_page(window)
+        sikumim_page()
     elif location == "premium":
-        premium_page(window)
+        premium_page()
+    elif location == "login":
+        login_page()
     elif location == "main":
-        main(window)
+        main()
 
 
-def sikumim_page(window):
+def login_page():
+    """Opens the login page"""
+    WINDOW.title(u"התחבר")
+    username = StringVar()
+    Entry(WINDOW, text=u"שם משתמש"[::-1], textvariable=username, width=26).grid(row=1, column=1, columnspan=2)
+    Button(WINDOW, text=u"שלח נתונים"[::-1], command=lambda: contact_server.create_socket(0)).grid(row=2, column=2)
+    Button(WINDOW, text=u"חזרה למסך הראשי"[::-1],
+           command=lambda: clean_page("main")).grid(row=2, column=1)
+
+
+
+def sikumim_page():
     """Opens the sikumim page"""
-    window.title(u"סיכומים")
-    Tkinter.Label(window, text=u"סיכומים"[::-1]).grid(row=1, column=1)
-    Tkinter.Button(WINDOW, text=u"חזרה למסך הראשי"[::-1],
-                   command=lambda: clean_page(WINDOW, "main")).grid(row=2, column=1)
-    window.mainloop()
+    WINDOW.title(u"סיכומים")
+    Label(WINDOW, text=u"סיכומים"[::-1]).grid(row=1, column=1)
+    Button(WINDOW, text=u"חזרה למסך הראשי"[::-1],
+           command=lambda: clean_page("main")).grid(row=2, column=1)
+    WINDOW.mainloop()
 
 
-def premium_page(window):
+def premium_page():
     """Open the "Buy Premium" page"""
-    window.title(u"קנה פרמיום")
-    Tkinter.Label(window, text=u"קנה פרמיום"[::-1]).grid(row=1, column=1)
-    Tkinter.Button(WINDOW, text=u"חזרה למסך הראשי"[::-1],
-                   command=lambda: clean_page(WINDOW, "main")).grid(row=2, column=1)
-    window.mainloop()
+    WINDOW.title(u"קנה פרמיום")
+    Label(WINDOW, text=u"קנה פרמיום"[::-1]).grid(row=1, column=1)
+    Button(WINDOW, text=u"חזרה למסך הראשי"[::-1],
+           command=lambda: clean_page("main")).grid(row=2, column=1)
+    WINDOW.mainloop()
 
 
-def main(window):
-    window.title(u"ברוכים הבאים לסיכומניאק!")
+def main():
+    WINDOW.geometry('225x200')
+    WINDOW.title(u"ברוכים הבאים לסיכומניאק!")
+    login = u"התחבר"
     sikumim = u"סיכומים"
     premium = u"קנה פרמיום"
-    Tkinter.Button(window, text=sikumim[::-1],
-                   command=lambda: clean_page(window, "sikumim")).grid(row=1, column=1)
-    Tkinter.Button(window, text=premium[::-1], fg="#ff0000",
-                   command=lambda: clean_page(window, "premium")).grid(row=2, column=1)
+    Button(WINDOW, text=login[::-1],
+           command=lambda : clean_page("login")).grid(row=1,column=1)
+    Button(WINDOW, text=sikumim[::-1],
+           command=lambda: clean_page("sikumim")).grid(row=1, column=2)
+    Button(WINDOW, text=premium[::-1], fg="#ff0000",
+           command=lambda: clean_page("premium")).grid(row=1, column=3)
     WINDOW.mainloop()
 
 
 if __name__ == "__main__":
-    main(WINDOW)
+    main()

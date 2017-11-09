df51027c2ef1f845f4c333b961b5dc2a86d53b76
SicuManyakOwner <SicuManyakOwner@gmail.com>
2017-11-09 10:30:19 +0200

    Test commit pycharm

--- Server/Server.py
+++ Server/Server.py
@@ -229,3 +229,5 @@ if __name__ == '__main__':
     initialize()
 
 
+#kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
+# TEST TEST TEST TEST TEST ETS TETS TETS TE STE TSHJHKSKSJDKFHK HLXK
